f01f82263cae47638f568818e9f4438618ad4440
Reinhardt von Brandis <u20461632@tuks.co.za>
2023-02-06 21:35:14 +0200

    clean new project

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-//! Plays animations from a skinned glTF.
-
 use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(AmbientLight {
-            color: Color::WHITE,
-            brightness: 1.0,
-        })
         .add_startup_system(setup)
         .run();
 }
 
 fn setup(mut commands: Commands) {
+    // Camera so we can look at things
     commands.spawn(Camera2dBundle::default());
 }
